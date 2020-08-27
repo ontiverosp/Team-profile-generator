3138a8135d8eca917cbd6fc670d35f7b0f3693e0
Pablo Ontiveros <pablacas@gmail.com>
2020-08-27 01:34:19 -0500

    Intern set up

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(id, name, email, school) {
+        if (!id) {
+            throw new Error("You are missing the id.");
+        }
+        if (!name) {
+            throw new Error("You are missing the name.");
+        }
+        if (!email) {
+            throw new Error("You are missing the email.");
+        }
+        if (!school) {
+            throw new Error("You are missing the school.");
+        }
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.school = school;
+    }
+    getSchool() {
+
+    }
+    getRole() {
+
+    }
+
+}
+
