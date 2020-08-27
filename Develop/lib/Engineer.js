424427b732acea910847e78e7ed6620ed42e8b3f
Pablo Ontiveros <pablacas@gmail.com>
2020-08-27 01:30:45 -0500

    engineer set up

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee{
+    constructor(id, name, email, github) {
+        if (!id) {
+            throw new Error("You are missing the id.");
+        }
+        if (!name) {
+            throw new Error("You are missing the name.");
+        }
+        if (!email) {
+            throw new Error("You are missing the email.");
+        }
+        if (!github) {
+            throw new Error("You are missing the github.");
+        }
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.github = github;
+    }
+
+
+getGithub(){
+
+}
+
+getRole() {
+
+}
+
+}
+
