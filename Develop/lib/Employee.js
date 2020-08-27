b8472079e20426671e3e6d6955618c3809b78880
Pablo Ontiveros <pablacas@gmail.com>
2020-08-27 01:25:32 -0500

    employee set up

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+
+    constructor(id, name, email) {
+        if (!id) {
+            throw new Error("You are missing the id.");
+        }
+        if (!name) {
+            throw new Error("You are missing the name.");
+        }
+        if (!email) {
+            throw new Error("You are missing the email.");
+        }
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    getName(){
+
+    }
+    getId(){
+
+    }
+    getEmail(){
+
+    }
+    getRole(){
+        
+    }
+}
+
